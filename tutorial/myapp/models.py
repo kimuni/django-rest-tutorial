58c6e6767e00ee14ca0ae041da9709fdbcd08bfe
kimuni <dbsldbsl486@hanmail.net>
2018-05-29 14:34:36 +0900

    django-rest-tutorial

---
+++ tutorial/myapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    image = models.ImageField(default='default_image.jpeg')
+
+
+
+# Create your models here.
